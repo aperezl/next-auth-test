5bb88a953f65f9498ac5e7d7b9b67143939f52b9
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-01-28 21:34:18 +0100

    chore: change babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -4,6 +4,6 @@ module.exports = {
     "babel-plugin-macros",
     ["babel-plugin-styled-components", { ssr: true }],
     ['@babel/plugin-proposal-class-properties', { loose: false }],
-    ["@babel/plugin-proposal-private-property-in-object", { loose: true }]
+    ["@babel/plugin-proposal-private-property-in-object", { loose: false }]
   ],
 };
